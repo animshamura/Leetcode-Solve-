38d4a1d3dcbdf64fd9d401575dbcb4973190b46d
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-21 01:17:02 +0600

    Update 144 - Binary Tree Preorder Traversal.cpp

--- 144 - Binary Tree Preorder Traversal.cpp
+++ 144 - Binary Tree Preorder Traversal.cpp
@@ -1,30 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    vector<int> con;
-    stack<TreeNode*> stk;
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
+ public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    preorder(root,ans);
+    return ans;
+  }
 
-        while(root || stk.size()){
-            while(root){
-                stk.push(root);
-                con.push_back(root->val);
-                root=root->left;
-            }
-            root=stk.top();
-            stk.pop();
-            root=root->right;
-        }
-        return con;
-    }
+ private:
+  void preorder(TreeNode* root, vector<int>& ans) {
+    if (root == NULL) return;
+    ans.push_back(root->val);
+    preorder(root->left,ans);
+    preorder(root->right,ans);
+  }
 };
