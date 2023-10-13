d498700f36d2ba931201df3514017c94b11f40d1
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2023-10-13 19:37:46 +0600

    Create 88. Merge Sorted Array.cpp

---
+++ 88. Merge Sorted Array.cpp
@@ -0,0 +1,16 @@
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m + n;
+        while (m+n > max(m,n)) {
+            if (nums1[m - 1] > nums2[n - 1]) nums1[i - 1] = nums1[m - 1], m--; 
+            else   nums1[i - 1] = nums2[n - 1],  n--;
+             i--;
+        }
+
+        while (n > 0) nums1[i - 1] = nums2[n - 1], n--, i--;
+        
+    }
+};
