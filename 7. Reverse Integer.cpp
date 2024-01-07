cef717cd4b95658ac783b0ce9ece104094581b9c
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2024-01-07 22:54:43 +0600

    Create 7. Reverse Integer.cpp

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,14 @@
+#define ll long long
+class Solution {
+ public:
+  int reverse(int x) {
+    ll ans = 0;
+
+    while (x != 0) {
+      ans = ans * 10 + x % 10;
+      x /= 10;
+    }
+
+    return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
+  }
+};
