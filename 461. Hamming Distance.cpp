0ed3a97df2d76b1e521893e36b1cd268bdf4e83c
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2024-01-09 12:39:49 +0600

    Create 461. Hamming Distance.cpp

---
+++ 461. Hamming Distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  int hammingDistance(int x, int y) {
+    int ans = 0;
+
+    while (x || y) {
+      ans += x&1 ^ y&1;
+      x/=2, y/=2;
+    }
+
+    return ans;
+  }
+};
